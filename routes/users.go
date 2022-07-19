47541aabf59214e7783f8c18413ffed44cab47d2
Isaac Folmsbee <if@sent.com>
2022-07-19 01:56:32 -0400

    Create user account w/ primary wallet

--- routes/users.go
+++ routes/users.go
@@ -3,6 +3,10 @@ package routes
 import (
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/stelofinance/api/database"
+	"github.com/stelofinance/api/models"
+	"github.com/stelofinance/api/tools"
+	"golang.org/x/crypto/bcrypt"
 )
 
 var validate = validator.New()
@@ -27,5 +31,31 @@ func postUser(c *fiber.Ctx) error {
 		})
 	}
 
-	return c.Status(200).JSON(body)
+	// Hash the password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(body.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"code": "S0000",
+		})
+	}
+
+	// Create the user model and insert into db
+	user := models.User{
+		Username: body.Username,
+		Password: string(hashedPassword),
+		Wallet: models.Wallet{
+			Address: tools.RandString(6),
+		},
+	}
+	result := database.Db.Create(&user)
+
+	// If error on insertion assume username was taken
+	// TODO: handle this cleaner, check error, maybe address already taken?
+	if result.Error != nil {
+		return c.Status(400).JSON(fiber.Map{
+			"code": "U0000",
+		})
+	}
+
+	return c.Status(201).SendString("User created")
 }
