eef26fb7f41bca46fc9406a2a4d1059916002e4a
Nintron <47835714+Nintron27@users.noreply.github.com>
2022-11-11 23:22:02 -0500

    Update another error response

--- routes/user.go
+++ routes/user.go
@@ -165,7 +165,7 @@ func putActiveWallet(c *fiber.Ctx) error {
 			WalletID: body.WalletID,
 		})
 		if rows == 0 {
-			return c.Status(400).SendString(constants.ErrorU002)
+			return c.Status(404).SendString(constants.ErrorU002)
 		}
 	}
 
